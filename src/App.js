407cacf3f9808bcc0182ff3ce5a765a4507d174e
Rafaelltm <rafaelltm10@hotmail.com>
2021-08-09 10:47:18 -0300

    adição da lib react-cytoscape para grafos
    Co-authored-by: abner423 <abner.f.c.r@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CytoscapeComponent from 'react-cytoscapejs';
+
 import Registration from './components/Registration/Registration';
 import Card from './components/Card/Card';
 
-import "./app.css"
+import "./app.css";
 
 function App() {
+
+  const elements = [
+    { data: { id: 'one', label: 'Node 1' }, position: { x: 350, y: 350 } },
+    { data: { id: 'two', label: 'Node 2' }, position: { x: 600, y: 600 } },
+    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+  ];
+
   return (
     <div className="container">
       <div className="div-lateral">
           <span className="titulo">Arvore Genealógica</span>
-
-          <Registration />
       </div>
       
       <div className="content">
-          <Card />
+      <CytoscapeComponent elements={elements} style={ { width: '100%', height: '100%' } } />
       </div>
     </div>
   );
